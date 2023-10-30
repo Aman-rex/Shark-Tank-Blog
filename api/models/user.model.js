50422d0ae78bc02af9e8f93ceb6f8af4aee6dc25
Aman-rex <15singh.aman@gmail.com>
2023-10-30 15:59:54 +0530

    Created test api route succesfully

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
-const Schema=mongoose.Schema
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    name:{type:string,required:true},
-    email:{type:string,required:true,unique:true},
-    password:{type:string,required:true},
-    role:{type:string,default:'User'}
-},{timestamps:true})
+const userSchema = new Schema(
+  {
+    name: { type: string, required: true },
+    email: { type: string, required: true, unique: true },
+    password: { type: string, required: true },
+    role: { type: string, default: "User" },
+  },
+  { timestamps: true }
+);
 
-
-const User = mongoose.model('User',userSchema)
-export default User
+const User = mongoose.model("User", userSchema);
+export default User;
